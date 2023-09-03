89f1e6c90afe08b53753f632174baba63fea7707
lucidrains <lucidrains@gmail.com>
2023-09-03 11:22:06 -0700

    add yet another function for automatically returning a finetune optimizer

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'perfusion-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.1.17',
+  version = '0.1.19',
   license='MIT',
   description = 'Perfusion - Pytorch',
   author = 'Phil Wang',
